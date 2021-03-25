55103feeabddd52f0be8dda99da3379f873bed70
zhuo.wang <zhuo.wang@opay-inc.com>
2021-03-25 23:31:56 +0800

    add diagram query func

--- teacher-core/src/main/java/com/melbsoft/teacherplatform/model/teacher/vo/DataQuery.java
+++ teacher-core/src/main/java/com/melbsoft/teacherplatform/model/teacher/vo/DataQuery.java
@@ -1,8 +1,10 @@
 package com.melbsoft.teacherplatform.model.teacher.vo;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.Data;
 
 @Data
 public class DataQuery {
+    @Parameter(description = "报表数据索引号", example = "1")
     long index;
 }
